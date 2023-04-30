54340a773aa6318468715787fc85853895d3451d
monsterchick <38910509+monsterchick@users.noreply.github.com>
2023-04-30 16:54:35 -0700

    Update querySelectorAll.js

--- JavaScript/querySelectorAll.js
+++ JavaScript/querySelectorAll.js
@@ -1,9 +1,9 @@
 url = "https://www.youtube.com/watch?v=wGBWAoLUIj4&list=PLwDQt7s1o9J6AnGvVWTwv2SkPY95nbgtQ"
 
-
+// 根据id和class遍历打印文本内容
+// method 1
 const elements = document.querySelectorAll('#video-title.style-scope.ytd-playlist-panel-video-renderer');	// style-scope.ytd-pl中的空格表示两者为不同的类，转为"."连接
 
-// method 1
 elements.forEach(element => {	  // =>用于定义匿名函数
   console.log(element.textContent.trim());
 });
