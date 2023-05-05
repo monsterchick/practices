b60acfc00022cdf4e7927f27c7ba00f601fe2a48
kwan <kwanho.app@gmail.com>
2023-05-05 13:54:22 +0100

    update

--- python/modules/tabulate/main.py
+++ python/modules/tabulate/main.py
@@ -1,2 +1,13 @@
-import tabulate
+from tabulate import tabulate
 
+data = [
+    ["John Doe", 25, "Male"],
+    ["Jane Smith", 32, "Female"],
+    ["Bob Johnson", 45, "Male"],
+]
+
+headers = ["Name", "Age", "Gender"]
+
+table = tabulate(data, headers, tablefmt="pipe")
+
+print(table)
