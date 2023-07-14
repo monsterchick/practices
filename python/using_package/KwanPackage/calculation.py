72e1258ff42020ac1c65957b6b1fde51f9bae95c
kwan <kwanho.app@gmail.com>
2023-07-15 06:43:29 +0800

    update

---
+++ python/using_package/KwanPackage/calculation.py
@@ -0,0 +1,6 @@
+from KwanPackage.sum import sum
+
+
+def cal(num1,num2):
+    result = sum(num1,num2)
+    print(result)
---
+++ python/using_package/main.py
@@ -0,0 +1,5 @@
+from KwanPackage import calculation
+num1 = 123
+num2 = 9789
+
+calculation.cal(num1,num2)
